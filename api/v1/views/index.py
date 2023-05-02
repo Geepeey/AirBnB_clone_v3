618ef1955264300008621830679ea48351dd3afa
Godspower Udokpoh <111070276+Geepeey@users.noreply.github.com>
2023-05-02 12:19:36 +0100

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,27 +3,23 @@
 """index file for flask app"""
 
 from api.v1.views import app_views
+from flask import Flask
 from flask import jsonify
 from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def api_status():
-    """a function to return api status"""
-
+    """return api status"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
-def obj_stats():
+def count():
     """returns the number of each object"""
-
-    my_dict = {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User")
-            }
-    return jsonify(my_dict)
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})

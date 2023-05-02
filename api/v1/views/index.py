eac447a39e51121bf20a7bd267cc5a7c80f6ebdf
Godspower Udokpoh <111070276+Geepeey@users.noreply.github.com>
2023-05-02 12:48:30 +0100

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
-"""Index file for flask app"""
 
-from models import storage
-from flask import Flask
+"""Index flask application file"""
+
 from api.v1.views import app_views
+from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
+from os import getenv
+from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """return JSON status"""
-    return jsonify({"status": "OK"})
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def count():
-    """Returns the number of each object"""
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+@app.teardown_appcontext
+def clean_up(exception=None):
+    """eliminates current Session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """404 error handler"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)

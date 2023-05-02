d263ca4842c2cc45e2c0e9521304cabe72b7dea6
Godspower Udokpoh <harshmallins@gmail.com>
2023-05-02 05:46:03 -0700

    App and Init fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,10 +14,10 @@ app.register_blueprint(app_views)
 def teardown_app_context(exception):
     """Closes session after each request"""
     storage.close()
-    
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
-    
+
     app.run(host=host, port=port, threaded=True)

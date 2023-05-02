ef66e8cf15479011dfea53dab29ed94b8110ae9a
Godspower Udokpoh <harshmallins@gmail.com>
2023-05-02 07:47:24 -0700

    ...

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,6 +7,7 @@ from models import storage
 from models.city import City
 from models.user import User
 from models.place import Place
+from models.amenity import Amenity
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
@@ -79,3 +80,54 @@ def update_place(place_id):
             setattr(place, key, value)
     storage.save()
     return jsonify(place.to_dict())
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def search_places():
+    """
+    Retrieves all Place objects depending of the JSON in the body of the request.
+    """
+    if not request.is_json:
+        return jsonify({"error": "Not a JSON"}), 400
+
+    data = request.get_json()
+
+    if not data:
+        places = storage.all(Place).values()
+        return jsonify([place.to_dict() for place in places])
+
+    states = data.get("states", [])
+    cities = data.get("cities", [])
+    amenities = data.get("amenities", [])
+
+    if not states and not cities and not amenities:
+        places = storage.all(Place).values()
+        return jsonify([place.to_dict() for place in places])
+
+    places = []
+    if states:
+        for state_id in states:
+            state = storage.get(State, state_id)
+            if not state:
+                continue
+            for city in state.cities:
+                places += city.places
+    if cities:
+        for city_id in cities:
+            city = storage.get(City, city_id)
+            if not city:
+                continue
+            if city not in [state_city for state in storage.all(State).values()
+                            for state_city in state.cities]:
+                places += city.places
+            else:
+                for place in city.places:
+                    if place not in places:
+                        places.append(place)
+    if amenities:
+        amenities_objs = [storage.get(Amenity, amenity_id)
+                          for amenity_id in amenities]
+        places = [place for place in places if all(
+            amenity in place.amenities for amenity in amenities_objs)]
+
+    return jsonify([place.to_dict() for place in places])

cb8de359917068139202d1f990513d12354f868b
Godspower Udokpoh <111070276+Geepeey@users.noreply.github.com>
2023-05-02 13:14:09 +0100

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
+"""Creates a Flask app and registers the app view"""
 
-"""flask application file"""
-
-from api.v1.views import app_views
 from flask import Flask
-from flask import jsonify
-from flask_cors import CORS
-from os import getenv
 from models import storage
-
+from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """close current session"""
+def teardown_app_context(exception):
+    """
+    Closes the storage engine after each request
+    """
     storage.close()
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    """404 error handler"""
-    return jsonify({"error": "Not found"}), 404
-
+    
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
-
-    app.run(host, int(port), threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)

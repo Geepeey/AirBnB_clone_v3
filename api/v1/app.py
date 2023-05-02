a1bb7377bd37a9048b7c70a5130dd9186b2a7145
Godspower Udokpoh <111070276+Geepeey@users.noreply.github.com>
2023-05-02 13:40:45 +0100

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,13 +12,12 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_app_context(exception):
-    """
-    Closes the storage engine after each request
-    """
+    """Closes session after each request"""
     storage.close()
     
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
+    
     app.run(host=host, port=port, threaded=True)
